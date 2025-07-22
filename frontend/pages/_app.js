f9ef51e61a462e43a50a4239a8c8615f3e9d53e7
Wajahat <wajahatcru@gmail.com>
2025-07-22 11:46:31 +0500

    add: navbar on every page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: '80px' }}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 } 
